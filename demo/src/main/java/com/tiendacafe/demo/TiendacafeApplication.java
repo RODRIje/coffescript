4c4f0ac1160e826fa82a43cad236a70deccb5522
RODRIje <129014673+RODRIje@users.noreply.github.com>
2024-05-20 19:38:08 -0300

    Update TiendacafeApplication.java
    Dejar main como v1

--- demo/src/main/java/com/tiendacafe/demo/TiendacafeApplication.java
+++ demo/src/main/java/com/tiendacafe/demo/TiendacafeApplication.java
@@ -13,19 +13,4 @@ public class TiendacafeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendacafeApplication.class, args);
 	}
-
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
-
 }
