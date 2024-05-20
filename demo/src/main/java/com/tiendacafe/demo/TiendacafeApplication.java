f4040a32c16aba98ef9c579f415554229fe5e33c
RODRIje <129014673+RODRIje@users.noreply.github.com>
2024-05-20 19:34:11 -0300

    Update TiendacafeApplication.java
    Update class miconfig

--- demo/src/main/java/com/tiendacafe/demo/TiendacafeApplication.java
+++ demo/src/main/java/com/tiendacafe/demo/TiendacafeApplication.java
@@ -14,4 +14,18 @@ public class TiendacafeApplication {
 		SpringApplication.run(TiendacafeApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+
 }
